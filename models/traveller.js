9f261fc4cb2c31242478f3f57b86f46da8b29b14
Madeleine Wood <madeleinecwood@gmail.com>
2020-10-08 07:40:40 +0100

    homework completed

---
+++ models/traveller.js
@@ -0,0 +1,41 @@
+const Traveller = function(journeys) {
+    this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function(journeys) {
+    const result = this.journeys.map((journey) => {
+        return journey.startLocation
+    });
+    return result
+};
+
+Traveller.prototype.getJourneyEndLocations = function() {
+    const result = this.journeys.map((journey) => {
+        return journey.endLocation
+    });
+    return result
+};
+
+Traveller.prototype.getJourneysByTransport = function(transport) {
+    const result = this.journeys.filter(journey => journey.transport === transport);
+    return result;
+};
+
+Traveller.prototype.getJourneysByMinDistance = function(minDistance) {
+    const result = this.journeys.filter(journey => journey.distance >= minDistance);
+    return result;
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function() {
+    const result = this.journeys.reduce((totalDistance, journey) => {
+        return totalDistance + journey.distance
+    }, 0);
+    return result
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function() {
+
+};
+
+
+module.exports = Traveller;
